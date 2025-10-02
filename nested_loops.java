be14619ef28f722c3948915b1fe2b88552cdf3fb
GUOXIAOJUN0908 <xiaojunguo21@gmail.com>
2025-10-02 12:00:17 +0800

    终于解锁嵌套for循环

---
+++ nested_loops.java
@@ -0,0 +1,25 @@
+public class nested_loops {
+
+    public static void main(String[] agrs){
+        for(int i=0;i<3;i++){
+            for(int j=1;j<4;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+class nested_loops_2 {
+    public static void main(String[] agrs){
+        int count =1;
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+
+        }
+        }
+    }
+
